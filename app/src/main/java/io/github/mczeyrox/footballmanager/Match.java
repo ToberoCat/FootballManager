b5f79498e4ca5e199ec0b39f7ed3593a349e9e55
McZeyrox <84139376+McZeyrox@users.noreply.github.com>
2021-11-13 16:18:11 +0100

    gui update

--- app/src/main/java/io/github/mczeyrox/footballmanager/Match.java
+++ app/src/main/java/io/github/mczeyrox/footballmanager/Match.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class Match extends AppCompatActivity {
@@ -13,8 +14,14 @@ public class Match extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
     }
-    public void endMatch(View view) {
+    public void MainMenu(View view) {
+        Log.d("ggg","back");
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+    public void startMatch(View view) {
+        Intent intent = new Intent(this, Running_Match.class);
+        startActivity(intent);
+    }
+
 }
