c43408755def454187072a745cf7df4cf7310470
McZeyrox <84139376+McZeyrox@users.noreply.github.com>
2021-11-20 10:18:25 +0100

    ui update

--- app/src/main/java/io/github/mczeyrox/footballmanager/MainActivity.java
+++ app/src/main/java/io/github/mczeyrox/footballmanager/MainActivity.java
@@ -27,4 +27,8 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Verein.class);
         startActivity(intent);
     }
+    public void enterShop(View view) {
+        Intent intent = new Intent(this, Shop.class);
+        startActivity(intent);
+    }
 }
